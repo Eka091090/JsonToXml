258ba61d4dccd5ba6745db90123cc45885b7d580
Eka091090 <grinik1990@icloud.com>
2024-11-10 17:59:42 +0700

    first commit

---
+++ JsonToXmlConverter.cs
@@ -0,0 +1,51 @@
+﻿using System.Text.Json;
+using System.Xml;
+
+class JsonToXmlConverter
+    {
+    public static void ConvertJsonToXml(JsonElement jsonElement,
+                                        XmlElement parentElement,
+                                        XmlDocument xmlDocument)
+    {
+        switch (jsonElement.ValueKind)
+        {
+            case JsonValueKind.Object:
+                foreach (var property in jsonElement.EnumerateObject())
+                {
+                    XmlElement element = xmlDocument.CreateElement(property.Name);
+                    parentElement.AppendChild(element);
+                    ConvertJsonToXml(property.Value, element, xmlDocument);
+                }
+                break;
+
+            case JsonValueKind.Array:
+                foreach (var item in jsonElement.EnumerateArray())
+                {
+                    XmlElement arrayItemElement = xmlDocument.CreateElement("Item");
+                    parentElement.AppendChild(arrayItemElement);
+                    ConvertJsonToXml(item, arrayItemElement, xmlDocument);
+                }
+                break;
+
+            case JsonValueKind.String:
+                parentElement.InnerText = jsonElement.GetString();
+                break;
+
+            case JsonValueKind.Number:
+                parentElement.InnerText = jsonElement.GetRawText();
+                break;
+
+            case JsonValueKind.True:
+            case JsonValueKind.False:
+                parentElement.InnerText = jsonElement.GetBoolean().ToString();
+                break;
+
+            case JsonValueKind.Null:
+                // Оставляем пустым для null
+                break;
+
+            default:
+                throw new NotSupportedException($"Тип значения {jsonElement.ValueKind} не поддерживается.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿﻿using System;
+using System.Text.Json;
+using System.Xml;
+class Program
+{
+    static void Main(string[] args)
+    {
+        if (args.Length != 1)
+        {
+            Console.WriteLine("Использование: JsonToXmlConverter.exe <json-файл>");
+            return;
+        }
+
+        string jsonFilePath = args[0];
+
+        try
+        {
+            string jsonString = System.IO.File.ReadAllText(jsonFilePath);
+            using JsonDocument jsonDocument = JsonDocument.Parse(jsonString);
+            XmlDocument xmlDocument = new XmlDocument();
+
+            XmlElement root = xmlDocument.CreateElement("Root");
+            xmlDocument.AppendChild(root);
+
+            JsonToXmlConverter.ConvertJsonToXml(jsonDocument.RootElement, root, xmlDocument);
+
+            string xmlOutput = xmlDocument.OuterXml;
+            Console.WriteLine("Результат XML:");
+            Console.WriteLine(xmlOutput);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Ошибка: {ex.Message}");
+        }
+    }
+}
